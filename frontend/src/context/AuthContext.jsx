42ae29c24b912614dfef17b21406523995c3c880
fendishatech <liyukibir@gmail.com>
2023-08-29 17:24:02 -0700

    auth context

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,34 @@
+import { createContext, useContext, useState } from "react";
+
+// CREATE CONTEXT
+const AuthContext = createContext({
+  user: null,
+  token: null,
+  setUser: () => {},
+  setToken: () => {},
+});
+
+// DEFINE PROVIDER
+export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState({});
+  const [token, _setToken] = useState(localStorage.getItem("ACCESS_TIME"));
+
+  const setToken = (token) => {
+    _setToken(token);
+
+    // set in local storage
+    if (token) {
+      localStorage.setItem("ACCESS_TOKEN", JSON.stringify(token));
+    } else {
+      localStorage.removeItem("ACCESS_TOKEN");
+    }
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, token, setUser, setToken }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(AuthContext);
